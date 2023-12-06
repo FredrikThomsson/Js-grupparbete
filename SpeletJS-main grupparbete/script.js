ca539405a6b3d1be978507160c8393097eff8eec
FredrikThomsson <117840316+FredrikThomsson@users.noreply.github.com>
2023-12-06 12:10:08 +0100

    Add files via upload

--- SpeletJS-main grupparbete/script.js
+++ SpeletJS-main grupparbete/script.js
@@ -1,8 +1,7 @@
 var timer;
 var selectedTime;
 const restartButton = document.getElementById("restart");
-let timeoutId;
-
+ 
 document.addEventListener('DOMContentLoaded', function () {
  
   const cards = document.querySelectorAll('.card');
@@ -31,22 +30,26 @@ document.addEventListener('DOMContentLoaded', function () {
     if (flippedCards.length === 2) {
       setTimeout(checkMatch, 1000);
     }
-
+ 
  
   }
   function checkMatch() {
     const [card1, card2] = flippedCards;
-    const img1 = card1.querySelector('.img-back6').src;
-    const img2 = card2.querySelector('.img-back1').src;
-
+    const img1 = card1.querySelector('.img-back').src;
+    const img2 = card2.querySelector('.img-back').src;
+ 
     if (img1 === img2) {
       // Matchning: lägg till klassen för matchade kort
       card1.classList.add('matched');
       card2.classList.add('matched');
-
+ 
       // Kontrollera om alla kort har matchats
       if (document.querySelectorAll('.matched').length === cards.length) {
-        alert('Congratulations! You matched all the cards.');
+        alert('Grattis du klarade spelet!');
+        
+        matchedCards.forEach(card => {
+          card.classList.remove('matched');
+        });
       }
     } else {
       // Ingen matchning: vänd tillbaka korten
@@ -57,63 +60,63 @@ document.addEventListener('DOMContentLoaded', function () {
         card2.querySelector('.back-view').style.display = 'none';
       }, 500);
     }
-
+ 
     // Återställ flippedCards-arrayen
     flippedCards = [];
 }
 });
-
+ 
 function updateTime() {
-    
+   
     var dropdown = document.getElementById("timeDropdown");
-
+ 
         // Kollar om timern är igång
         if (timer) {
             clearInterval(timer);
         }
-
+ 
     // uppdaterar den valda tiden
     selectedTime = dropdown.options[dropdown.selectedIndex].value;
-
+ 
     // uppdaterar valda tiden i .details och .time
     var detailsParagraph = document.querySelector(".details .time span b");
     if (detailsParagraph) {
         detailsParagraph.innerText = selectedTime + "s";
     }
 }
-
+ 
 function startOm() {
-  clearInterval(timer);
-  clearInterval(timeoutId); 
-  startTimer();
-
-  const frontViews = document.querySelectorAll('.front-view');
-  const backViews = document.querySelectorAll('.back-view');
-
-  frontViews.forEach(view => {
-    view.style.display = 'block';
-  });
-
-  backViews.forEach(view => {
-    view.style.display = 'none';
-  });
-}
-
-
-
+    clearInterval(timer);
+    startTimer();
+ 
+    const frontViews = document.querySelectorAll('.front-view');
+    const backViews = document.querySelectorAll('.back-view');
+ 
+    frontViews.forEach(view => {
+      view.style.display = 'block';
+    });
+ 
+    backViews.forEach(view => {
+      view.style.display = 'none';
+    });
+  }
+ 
+ 
+ 
+ 
 function startTimer() {
-
+ 
     var timeLeft = selectedTime;
     timer = setInterval(function () {
         // räknar ner tiden
         timeLeft--;
-
+ 
         // uppdaterar tiden som är kvar
         var detailsParagraph = document.querySelector(".details .time span b");
         if (detailsParagraph) {
             detailsParagraph.innerText = timeLeft + "s";
         }
-
+ 
         // när tiden når 0 skrivs game over
         if (timeLeft === 0) {
             clearInterval(timer);
@@ -121,7 +124,7 @@ function startTimer() {
         }
     }, 1000); // hur snabbt nedräkningen sker
 }
-
-
+ 
+ 
 updateTime();
 startTimer();
